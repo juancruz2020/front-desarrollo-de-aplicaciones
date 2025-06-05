3df49c9dddca975cf5e4ae2959b46ddce0d13251
Maru524 <147671431+Maru524@users.noreply.github.com>
2025-06-05 15:58:57 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
