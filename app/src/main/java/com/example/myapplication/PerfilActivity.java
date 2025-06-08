5102556b5875bbe79f883095ee0005ef05e683d1
Maru524 <147671431+Maru524@users.noreply.github.com>
2025-06-08 14:15:57 -0300

    Update PerfilActivity.java

--- app/src/main/java/com/example/myapplication/PerfilActivity.java
+++ app/src/main/java/com/example/myapplication/PerfilActivity.java
@@ -14,20 +14,27 @@ public class PerfilActivity extends AppCompatActivity {
         String tipoUsuario = "alumno"; // puede ser: "visitante", "usuario" o "alumno"
 
         switch (tipoUsuario) {
-           case "visitante":
+            case "visitante":
                 setContentView(R.layout.activity_perfil_visitante);
+                //iniciarSesion
                 Intent inicioSesion = new Intent(this, Login.class);
-                startActivity(inicioSesion);
+                findViewById(R.id.btnIniciarSesion).setOnClickListener( v -> startActivity(inicioSesion));
                 break;
             case "usuario":
                 setContentView(R.layout.activity_perfil_usuario);
+                //pasar de Usuario a Alumno
                 Intent usuarioAlumno = new Intent(this, RegistroAlumno.class);
-                startActivity(usuarioAlumno);
+                findViewById(R.id.btnConvertirseAlumno).setOnClickListener(v -> startActivity(usuarioAlumno));
                 break;
             case "alumno":
                 setContentView(R.layout.activity_perfil_alumno);
                 break;
         }
+
+        // Botón volver
+        findViewById(R.id.btnBack).setOnClickListener(v -> finish());
+
+    }
 
         // Botón volver
         findViewById(R.id.btnBack).setOnClickListener(v -> finish());
