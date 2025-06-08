de5001f09f880fdc064f11681a1f53465eab0a6a
Maru524 <147671431+Maru524@users.noreply.github.com>
2025-06-08 14:23:28 -0300

    Update PerfilActivity.java

--- app/src/main/java/com/example/myapplication/PerfilActivity.java
+++ app/src/main/java/com/example/myapplication/PerfilActivity.java
@@ -13,28 +13,31 @@ public class PerfilActivity extends AppCompatActivity {
         // Simular carga desde backend
         String tipoUsuario = "alumno"; // puede ser: "visitante", "usuario" o "alumno"
 
+        Intent cerrarSesion = new Intent(this, Login.class);
+
         switch (tipoUsuario) {
             case "visitante":
                 setContentView(R.layout.activity_perfil_visitante);
                 //iniciarSesion
                 Intent inicioSesion = new Intent(this, Login.class);
                 findViewById(R.id.btnIniciarSesion).setOnClickListener( v -> startActivity(inicioSesion));
+                //crearCuenta
+                Intent crearCuenta = new Intent(this, Registro.class);
+                findViewById(R.id.btnCrearCuenta).setOnClickListener(v -> startActivity(crearCuenta));
                 break;
             case "usuario":
                 setContentView(R.layout.activity_perfil_usuario);
                 //pasar de Usuario a Alumno
                 Intent usuarioAlumno = new Intent(this, RegistroAlumno.class);
                 findViewById(R.id.btnConvertirseAlumno).setOnClickListener(v -> startActivity(usuarioAlumno));
+                findViewById(R.id.btnCerrarSesion).setOnClickListener(v -> startActivity(cerrarSesion));
+
                 break;
             case "alumno":
                 setContentView(R.layout.activity_perfil_alumno);
+                findViewById(R.id.btnCerrarSesion).setOnClickListener(v -> startActivity(cerrarSesion));
                 break;
         }
-
-        // Botón volver
-        findViewById(R.id.btnBack).setOnClickListener(v -> finish());
-
-    }
 
         // Botón volver
         findViewById(R.id.btnBack).setOnClickListener(v -> finish());
