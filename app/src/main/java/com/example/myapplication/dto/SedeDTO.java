33795068c5be928c0222a20b1e8c70f9a2047c0a
IvanCu <cupitoivan@gmail.com>
2025-07-06 23:33:19 -0300

    version vieja actualizada

--- app/src/main/java/com/example/myapplication/dto/SedeDTO.java
+++ app/src/main/java/com/example/myapplication/dto/SedeDTO.java
@@ -9,13 +9,16 @@ public class SedeDTO implements Serializable {
     public String telefono;
     public String dias;
     public String horarios;
+    public int imagenResId;
+    
 
-    public SedeDTO(String nombre, String descripcion, String direccion, String telefono, String dias, String horarios) {
+    public SedeDTO(String nombre, String descripcion, String direccion, String telefono, String dias, String horarios, int imagenResId) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.direccion = direccion;
         this.telefono = telefono;
         this.dias = dias;
         this.horarios = horarios;
+        this.imagenResId = imagenResId;
     }
 }
